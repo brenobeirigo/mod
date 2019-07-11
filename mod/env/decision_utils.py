0ea5eca0fd92bdeda53c09087be1856d6bdf008f
brenobeirigo <brenobeirigo@gmail.com>
2019-07-11 15:25:24 +0200

    Match cars and trips in the same neighborhood or center

--- mod/env/decision_utils.py
+++ mod/env/decision_utils.py
@@ -193,6 +193,115 @@ def get_decision_set(
 
     return decisions
 
+def get_decision_set_classed3(
+    env,
+    trips,
+    level_id_cars_dict,
+    level_id_trips_dict,
+    rebalance_targets_dict,
+    max_battery_level=None,
+):
+    """Get list of decision tuples.
+    
+    Parameters
+    ----------
+    cars : list
+        Owned fleet
+    hired_cars : list
+        Third-party fleet
+    level_id_cars_dict : dict
+        For each tuple (level, id), list of cars
+    level_id_trips_dict : dict
+        For each tuple (level, id), list of trips
+    rebalance_targets_dict : dict()
+        List of reachable points from each id
+    max_battery_level : int, optional
+        If declared, add recharge decisions, by default None
+    
+    Returns
+    -------
+    list, list
+        List of all decision tuples and list of trip decisions for each
+        related to each class.
+    """
+
+    decisions = set()
+    decision_class = defaultdict(list)
+
+    for car in env.available+env.available_hired:
+        # Stay ####################################################### #
+        decisions.add(stay_decision(car))
+
+        # Rebalancing ################################################ #
+        rebalance_targets = rebalance_targets_dict[car.point.id]
+        decisions.update(rebalance_decisions(car, rebalance_targets, env))
+
+        # Recharge ################################################### #
+        if max_battery_level and car.battery_level < max_battery_level:
+            decisions.add(recharge_decision(car))
+
+
+        if env.config.match_neighbors():
+            
+            regions = env.get_zone_neighbors(
+                car.point,
+                level=(env.config.match_level,),
+                n_neighbors=(env.config.match_max_neighbors,)
+            )
+
+            regions.add(car.point.id_level(env.config.match_level))
+
+            # print(car, regions)
+
+        for trip in trips:
+            
+            # Skip trips not in the car neighborhood
+            if env.config.match_neighbors() and trip.o.id_level(env.config.match_level) not in regions:
+                continue
+
+            # Car and trip are not in the same area
+            if (
+                env.config.match_in_center() and
+                car.point.id_level(env.config.match_level) != trip.o.id_level(env.config.match_level)
+            ):
+                continue
+
+            # Car cannot service trip because it cannot go back
+            # to origin in time
+            if isinstance(car, HiredCar) and not env.can_move(
+                car.point.id,
+                trip.o.id,
+                trip.d.id,
+                car.depot.id,
+                car.contract_duration,
+            ):
+                continue
+
+            # Time to reach trip origin
+            travel_time = env.get_travel_time_od(car.point, trip.o)
+
+            # Can the car reach the trip origin?
+            if travel_time <= trip.max_delay:
+                
+                 # Setup decisions
+                d = trip_decision(car, trip)
+                decisions.add(d)
+                
+                # Car can fulfill the shortest delay
+                if travel_time <= trip.min_delay:
+                    
+                    # ---------------------------------------- #
+                    # DECISIONS ASSOCIATED TO EACH SQ CLASS ## #
+                    # ---------------------------------------- #
+
+                    # There might be repeated decisions
+                    # associated to the same class since
+                    # several trips can depart from the same
+                    # place.
+
+                    decision_class[trip.sq_class].append(d)
+
+    return decisions, decision_class
 
 def get_decision_set_classed(
     env,

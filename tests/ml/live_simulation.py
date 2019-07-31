da61a452f111a18e0762d2ff5ef8691bb340b584
brenobeirigo <brenobeirigo@gmail.com>
2019-07-31 21:33:24 +0200

    Added config module

--- tests/ml/live_simulation.py
+++ tests/ml/live_simulation.py
@@ -7,7 +7,14 @@ sys.path.append(root)
 
 from mod.env.simulator import PlotTrack
 import mod.ml.adp_network_server as alg
+import mod.env.config as config
 
-start_config = alg.get_sim_config({})
+start_config = alg.get_sim_config(
+    {
+        config.Config.TEST_LABEL: "SIM",
+        config.Config.FLEET_SIZE: 1500,
+        config.Config.DEMAND_RESIZE_FACTOR: 1,
+    }
+)
 run_plot = PlotTrack(start_config)
 run_plot.start_animation(alg.alg_adp)

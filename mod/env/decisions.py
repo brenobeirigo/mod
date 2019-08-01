7d242d4fbe38d9475c65cb8fc58ef825e6b166db
brenobeirigo <brenobeirigo@gmail.com>
2019-08-01 12:32:02 +0200

    Isolated decision generation

---
+++ mod/env/decisions.py
@@ -0,0 +1,176 @@
+from collections import defaultdict
+from mod.env.car import HiredCar
+
+# Decision codes
+
+TRIP_DECISION = "TRIP"
+
+STAY_DECISION = "STAY"
+
+RECHARGE_DECISION = "CHAR"
+
+REBALANCE_DECISION = "REBA"
+
+HIRE_DECISION = "HIRE"
+
+
+def stay_decision(car):
+    return (
+        (STAY_DECISION,)
+        + car.attribute
+        + (car.point.id,)
+        + (car.point.id,)
+        + ("_",)
+    )
+
+
+def recharge_decision(car):
+    return (
+        (RECHARGE_DECISION,)
+        + car.attribute
+        + (car.point.id,)
+        + (car.point.id,)
+        + ("_",)
+    )
+
+
+def rebalance_decision(car, neighbor):
+    return (
+        (REBALANCE_DECISION,)
+        + car.attribute
+        + (car.point.id,)
+        + (neighbor,)
+        + ("_",)
+    )
+
+
+def rebalance_decisions(car, targets, env):
+    rebalance_decisions = set()
+    for t in targets:
+        # Car cannot service trip because it cannot go back
+        # to origin in time
+        if isinstance(car, HiredCar) and not env.can_move(
+            car.point.id, car.point.id, t, car.depot.id, car.contract_duration
+        ):
+            continue
+
+        rebalance_decisions.add(rebalance_decision(car, t))
+    return rebalance_decisions
+
+
+def trip_decision(car, trip):
+    return (
+        (TRIP_DECISION,)
+        + car.attribute
+        + (trip.o.id,)
+        + (trip.d.id,)
+        + (trip.sq_class,)
+    )
+
+
+# #################################################################### #
+# Manipulate decisions ############################################### #
+# #################################################################### #
+
+
+def get_decisions(env, trips, min_battery_level=None):
+    """Get list of decision tuples.
+
+    Parameters
+    ----------
+    env : Amod object
+        Amod environment
+    trips : list
+        Trips placed in the current time step
+    min_battery_level : int, optional
+        Create recharging decisions with car battery level is lower, by
+        default None
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+
+    decisions = set()
+    decision_class = defaultdict(list)
+    # Which positions are surrounding each car position
+    attribute_rebalance = dict()
+
+    # ##################################################################
+    # SORT CARS ########################################################
+    # ##################################################################
+
+    for car in env.available + env.available_hired:
+        # Stay ####################################################### #
+        decisions.add(stay_decision(car))
+
+        # Rebalancing ################################################ #
+        try:
+            # Get rebalance targets if previously defined (car with
+            # the same attribute)
+            rebalance_targets = attribute_rebalance[car.point.id]
+
+        except:
+
+            # Get immediate neighbors (intersections) at reach degrees
+            if env.config.rebalance_reach:
+                rebalance_targets = env.get_neighbors(
+                    car.point, reach=env.config.rebalance_reach
+                )
+
+            # Get region center neighbors
+            else:
+                rebalance_targets = env.get_zone_neighbors(
+                    car.point,
+                    level=env.config.rebalance_level,
+                    n_neighbors=env.config.n_neighbors,
+                )
+
+            # All points a car can rebalance to from its corrent point
+            attribute_rebalance[car.point.id] = rebalance_targets
+
+        decisions.update(rebalance_decisions(car, rebalance_targets, env))
+
+        # Recharge ################################################### #
+        if min_battery_level and car.battery_level < min_battery_level:
+            decisions.add(recharge_decision(car))
+
+        for trip in trips:
+
+            # Car cannot service trip because it cannot go back
+            # to origin in time
+            if isinstance(car, HiredCar) and not env.can_move(
+                car.point.id,
+                trip.o.id,
+                trip.d.id,
+                car.depot.id,
+                car.contract_duration,
+            ):
+                continue
+
+            # Time to reach trip origin
+            travel_time = env.get_travel_time_od(car.point, trip.o)
+
+            # Can the car reach the trip origin?
+            if travel_time <= trip.max_delay:
+
+                # Setup decisions
+                d = trip_decision(car, trip)
+                decisions.add(d)
+
+                # Car can fulfill the shortest delay
+                if travel_time <= trip.min_delay:
+
+                    # ---------------------------------------- #
+                    # DECISIONS ASSOCIATED TO EACH SQ CLASS ## #
+                    # ---------------------------------------- #
+
+                    # There might be repeated decisions
+                    # associated to the same class since
+                    # several trips can depart from the same
+                    # place.
+
+                    decision_class[trip.sq_class].append(d)
+
+    return decisions, decision_class

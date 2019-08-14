a351d21527f19ed8f04cafcc485c3ba9b22bc670
brenobeirigo <brenobeirigo@gmail.com>
2019-08-14 08:35:59 +0200

    Inserted tuning parameters Method and MIPFocus

--- mod/env/matching.py
+++ mod/env/matching.py
@@ -423,6 +423,24 @@ def service_trips(
     else:
         # Disables all logging (file and console)
         m.setParam("OutputFlag", 0)
+        # Model is deterministic (usefull for testing)
+        m.setParam("Seed", 1)
+
+        # https://www.gurobi.com/documentation/8.1/refman/method.html#parameter:Method
+        # -1=automatic, 0=primal simplex, 
+        # 1=dual simplex, 
+        # 2=barrier, 
+        # 3=concurrent, 
+        # 4=deterministic concurrent, 
+        # 5=deterministic concurrent simplex.
+        m.setParam("Method", 1)
+        
+        # https://www.gurobi.com/documentation/8.1/refman/mipfocus.html
+        # If you are more interested in finding feasible solutions quickly, you can select MIPFocus=1.
+        # If you believe the solver is having no trouble finding good quality solutions, and wish to focus more attention on proving optimality, select MIPFocus=2.
+        # If the best objective bound is moving very slowly (or not at all), you may want to try MIPFocus=3 to focus on the bound. 
+        m.setParam("MIPFocus", 1)
+
 
     # ##################################################################
     # SORT CARS ########################################################

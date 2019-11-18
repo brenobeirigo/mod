22b3b9d407af883a2d1957093032349e3a2de061
brenobeirigo <brenobeirigo@gmail.com>
2019-11-18 17:21:44 +0100

    Update adhoc_compare.py

--- tests/ml/adhoc_compare.py
+++ tests/ml/adhoc_compare.py
@@ -7,7 +7,6 @@ root = os.getcwd().replace("\\", "/")
 sys.path.append(root)
 
 
-
 import mod.env.config as conf
 from mod.env.config import ConfigNetwork
 import pandas as pd
@@ -18,12 +17,10 @@ from pprint import pprint
 if __name__ == "__main__":
 
     adhoc_compare = [
-        "concentric_LIN_cars=0300-0000(L)_t=1_levels[3]=(1-0, 3-300, 3-600)_rebal=([0-8, 1-8][tabu=00])[L(05)][P]_[05h,+30m+04h+60m]_0.10(S)_1.00_0.10",
-        "concentric_LIN_cars=0300-0000(R)_t=1_levels[3]=(1-0, 3-300, 3-600)_rebal=([0-8, 1-8][tabu=00])[L(05)][P]_[05h,+30m+04h+60m]_0.10(S)_1.00_0.10",
-        "concentric_LIN_cars=0300-0000(R)_t=1_levels[3]=(1-0, 3-300, 3-600)_rebal=([0-8][tabu=00])[L(05)][P]_[05h,+30m+04h+60m]_0.10(S)_1.00_0.10",
-        "disjoint_LIN_cars=0300-0000(L)_t=1_levels[3]=(1-0, 3-300, 3-600)_rebal=([0-8, 1-8][tabu=00])[L(05)][P]_[05h,+30m+04h+60m]_0.10(S)_1.00_0.10",
-        "disjoint_LIN_cars=0300-0000(R)_t=1_levels[3]=(1-0, 3-300, 3-600)_rebal=([0-8, 1-8][tabu=00])[L(05)][P]_[05h,+30m+04h+60m]_0.10(S)_1.00_0.10",
-        "disjoint_LIN_cars=0300-0000(R)_t=1_levels[3]=(1-0, 3-300, 3-600)_rebal=([0-8][tabu=00])[L(05)][P]_[05h,+30m+04h+60m]_0.10(S)_1.00_0.10"
+        "TABU_LIN_cars=0300-0000(R)_t=1_levels[3]=(1-0, 3-300, 3-600)_rebal=([0-8, 1-8][tabu=00])[L(05)][P]_[05h,+30m+04h+60m]_0.10(S)_1.00_0.10",
+        "ANN_LIN_cars=0300-0000(L)_t=1_levels[3]=(1-0, 3-300, 3-600)_rebal=([0-8, 1-8][tabu=00])[L(05)][P]_[05h,+30m+04h+60m]_match=15_0.10(S)_1.00_0.10",
+        "ANN_LIN_cars=0300-0000(L)_t=1_levels[3]=(1-0, 3-300, 3-600)_rebal=([0-8, 1-8][tabu=10])[L(05)][P]_[05h,+30m+04h+60m]_match=15_0.10(S)_1.00_0.10",
+        "ANN_P_LIN_cars=0300-0000(L)_t=1_levels[3]=(1-0, 3-300, 3-600)_rebal=([0-8, 1-8][tabu=10])[L(05)][P]_[05h,+30m+04h+60m]_match=15_0.10(S)_1.00_0.10"
     ]
 
     ITERATIONS = 500
@@ -33,12 +30,12 @@ if __name__ == "__main__":
         for exp in adhoc_compare:
             path_all_stats = conf.FOLDER_OUTPUT + exp + "/overall_stats.csv"
             config_exp = ConfigNetwork()
-            config_exp.load( conf.FOLDER_OUTPUT + exp + "/exp_settings.json")
+            config_exp.load(conf.FOLDER_OUTPUT + exp + "/exp_settings.json")
 
             df = pd.read_csv(path_all_stats)
             # spatiotemporal_levels = exp[2].get_levels()
             # neighbors = exp[2].get_reb_neighbors()
-            id_label = exp # spatiotemporal_levels + neighbors
+            id_label = exp  # spatiotemporal_levels + neighbors
             d["reward_" + id_label] = df["Total reward"][:ITERATIONS]
             d["service_rate_" + id_label] = df["Service rate"][:ITERATIONS]
             d["time_" + id_label] = df["time"][:ITERATIONS]

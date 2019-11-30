00204959c5fff7c58b83ac214ded571de0f47d12
brenobeirigo <brenobeirigo@gmail.com>
2019-11-30 18:54:09 +0100

    Update live_simulation.py

--- tests/ml/live_simulation.py
+++ tests/ml/live_simulation.py
@@ -14,10 +14,11 @@ from mod.env.trip import ClassedTrip
 # best_link_LIN_cars=0500(L)_t=0.5_levels[3]=(3-0, 3-300, 3-600)_rebal=(0-4)[P(10)]_[05h,+15m+04h+30m]_0.10(S)_1.00_0.10
 start_config = alg.get_sim_config(
     {
-        ConfigNetwork.TEST_LABEL: "VAIKRAI",
+        ConfigNetwork.TEST_LABEL: "best_same_day_record",
         ConfigNetwork.DISCOUNT_FACTOR: 1,
-        ConfigNetwork.PENALIZE_REBALANCE: True,
+        
         ConfigNetwork.FLEET_SIZE: 300,
+        # DEMAND ############################################# #
         ConfigNetwork.DEMAND_RESIZE_FACTOR: 0.1,
         ConfigNetwork.DEMAND_TOTAL_HOURS: 4,
         ConfigNetwork.DEMAND_EARLIEST_HOUR: 5,
@@ -25,33 +26,60 @@ start_config = alg.get_sim_config(
         ConfigNetwork.OFFSET_REPOSITIONING_MIN: 30,
         ConfigNetwork.TIME_INCREMENT: 1,
         ConfigNetwork.DEMAND_SAMPLING: True,
-        ConfigNetwork.SQ_GUARANTEE: False,
-        ConfigNetwork.MAX_CARS_LINK: 5,
-        # 10 steps = 5 min
-        ConfigNetwork.TIME_MAX_CARS_LINK: 5,
-        ConfigNetwork.LINEARIZE_INTEGER_MODEL: False,
-        ConfigNetwork.USE_ARTIFICIAL_DUALS: False,
-        # Controlling user matching
+
+        # Service quality
         ConfigNetwork.MATCHING_DELAY: 15,
         ConfigNetwork.ALLOW_USER_BACKLOGGING: False,
-        ConfigNetwork.MAX_IDLE_STEP_COUNT: None,
+        ConfigNetwork.SQ_GUARANTEE: False,
+
+        # ADP EXECUTION ###################################### #
+        ConfigNetwork.MYOPIC: False,
+        # Rebalance costs are ignored by MIP but included when
+        # realizing the model
+        ConfigNetwork.POLICY_RANDOM: False,
+        ConfigNetwork.ADP_IGNORE_ZEROS: True,
+        ConfigNetwork.LINEARIZE_INTEGER_MODEL: False,
+        ConfigNetwork.USE_ARTIFICIAL_DUALS: False,
+        
+        # EXPLORATION ######################################## #
+        # ANNEALING + THOMPSON
         # If zero, cars increasingly gain the right of stay
         # still. This obliges them to rebalance consistently.
+        # If None, disabled.
         ConfigNetwork.IDLE_ANNEALING: None,
+        ConfigNetwork.ACTIVATE_THOMPSON: False,
+        ConfigNetwork.MAX_TARGETS: 6,
+
         # When cars start in the last visited point, the model takes
         # a long time to figure out the best time
-        ConfigNetwork.FLEET_START: conf.FLEET_START_LAST,
-        ConfigNetwork.CAR_SIZE_TABU: 10,
+        ConfigNetwork.FLEET_START: conf.FLEET_START_RANDOM,
+        ConfigNetwork.CAR_SIZE_TABU: 0,
+        # If REACHABLE_NEIGHBORS is True, then PENALIZE_REBALANCE
+        # is False
+        ConfigNetwork.PENALIZE_REBALANCE: True,
         ConfigNetwork.REACHABLE_NEIGHBORS: False,
-        ConfigNetwork.ADP_IGNORE_ZEROS: True,
+        ConfigNetwork.N_CLOSEST_NEIGHBORS: (
+            #(0, 8),
+            (1, 8),
+            (2, 4),
+            # (3, 4),
+            
+        ),
+        
+        # FLEET ############################################## #
+        # Car operation
+        ConfigNetwork.MAX_CARS_LINK: 5,
+        ConfigNetwork.MAX_IDLE_STEP_COUNT: None,
+        ConfigNetwork.TIME_MAX_CARS_LINK: 5,
+        # FAV configuration
         ConfigNetwork.DEPOT_SHARE: None,
         ConfigNetwork.FAV_DEPOT_LEVEL: 2,
         ConfigNetwork.FAV_FLEET_SIZE: 0,
         ConfigNetwork.SEPARATE_FLEETS: False,
-        ConfigNetwork.MYOPIC: False,
         # ConfigNetwork.PARKING_RATE_MIN = 1.50/60 # 1.50/h
         # ConfigNetwork.PARKING_RATE_MIN = 0.1*20/60,  # = rebalancing 1 min
         ConfigNetwork.PARKING_RATE_MIN: 0,  # = rebalancing 1 min
+        
     }
 )
 

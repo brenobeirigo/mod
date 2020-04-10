4818e8ed1ae462b94a32af6cd5590f2110ebdc0d
brenobeirigo <brenobeirigo@gmail.com>
2020-04-10 08:00:27 +0200

    Add service rate

--- mod/visual/slide_episode.py
+++ mod/visual/slide_episode.py
@@ -11,7 +11,14 @@ import numpy as np
 
 from bokeh.io import curdoc
 from bokeh.layouts import row, column
-from bokeh.models import ColumnDataSource, Label, Title, Legend
+from bokeh.models import (
+    ColumnDataSource,
+    Label,
+    Title,
+    Legend,
+    LinearAxis,
+    Range1d,
+)
 from bokeh.models.widgets import Slider, TextInput
 from bokeh.plotting import figure
 from tornado import gen
@@ -44,10 +51,10 @@ doc = curdoc()
 # exp_name = "DF_LIN_V=0400-0000(R)_I=5_L[4]=(10-0-, 11-0-, 12-0-, 13-0-)_R=([1-6, 2-6, 3-3][L(05)]_T=[06h,+60m+06h+30m]_1.00(S)_1.00_0.10_A_4.80_5.00_5.00_4.80_0.00_B_7.20_10.00_0.00_0.00_1.00"
 # exp_name = "B15_LIN_V=0400-0000(R)_I=5_L[4]=(10-0-, 11-0-, 12-0-, 13-0-)_R=([1-6][L(05)]_T=[06h,+60m+06h+30m]_1.00(S)_1.00_0.10_A_4.80_5.00_5.00_4.80_0.00_B_7.20_15.00_0.00_0.00_1.00"
 # exp_name = "MP_LIN_C2_V=0300-0000(R)_I=10_L[3]=(11-0-, 12-0-, 13-0-)_R=([1-6]_T=[06h,+0m+06h+0m]_0.10(S)_1.00_0.10_A_4.80_5.00_5.00_4.80_0.00_B_2.40_15.00_0.00_0.00_1.00"
-exp_name = "R_LIN_C2_V=0300-0000(R)_I=10_L[3]=(11-0-, 12-0-, 13-0-)_R=([1-6]_T=[06h,+30m+06h+30m]_0.10(S)_1.00_0.10_A_4.80_5.00_5.00_4.80_0.00_B_2.40_15.00_0.00_0.00_1.00"
+exp_name = "S_LIN_C1_V=0400-0000(R)_I=5_L[3]=(01-0-, 02-0-, 03-0-)_R=([1-6, 2-6][L(05)]_T=[06h,+30m+06h+30m]_0.10(S)_1.00_0.10_A_2.40_10.00_0.00_0.00_P_B_2.40_10.00_0.00_0.00_P"
 # method = "reactive"
-method = "optimal"
-# method = "adp/train"
+# method = "optimal"
+method = "adp/train"
 path_fleet = f"C:/Users/LocalAdmin/OneDrive/leap_forward/phd_project/reb/code/mod/data/output/{exp_name}/{method}/fleet/data/"
 path_demand = f"C:/Users/LocalAdmin/OneDrive/leap_forward/phd_project/reb/code/mod/data/output/{exp_name}/{method}/service/data/"
 path_overall_stats = f"C:/Users/LocalAdmin/OneDrive/leap_forward/phd_project/reb/code/mod/data/output/{exp_name}/{method}/overall_stats.csv"
@@ -62,6 +69,12 @@ color_fleet_status = {
     "Returning": "#e55215",
     "Driving to pick up": "#e55215",
     "Total": "magenta",
+}
+
+# Color per vehicle status
+color_overall_status = {
+    "Service rate": "#24aafe",
+    "Total reward": "red",
 }
 
 # Color per vehicle status
@@ -224,22 +237,68 @@ def configure_plot_fleet(p, status_list):
 
 def configure_overall_stats(p, status_list):
 
+    # Setup title
+    title = Title(
+        align="center",
+        text="Overall performance",
+        text_font_size="16pt",
+        text_color="#929292",
+        text_font_style="normal",
+    )
+
+    # Add title
+    p.title = title
+
+    # Set x axis settings
+    p.xaxis.axis_label = "Iteration"
+    p.xaxis.major_label_text_font_size = axes_font_size
+    p.xaxis.axis_label_text_font_size = axes_font_size
+
+    # Set y axis settings
+    p.yaxis[0].axis_label = "Cost"
+    p.yaxis.major_label_text_font_size = axes_font_size
+    p.yaxis.axis_label_text_font_size = axes_font_size
+
+    # Setting the second y axis range name and range
+    plot_iterations.extra_y_ranges = {"Service rate": Range1d(start=0, end=1)}
+
+    # Adding the second axis to the plot
+    plot_iterations.add_layout(
+        LinearAxis(y_range_name="Service rate", axis_label="Service rate"),
+        "right",
+    )
+
     # Save glyph per status
     items = dict()
-    for status in status_list:
-        data = ColumnDataSource(data=dict(x=[], y=[]))
-        source_overall_stats[status] = data
-        line = p.line(
-            "x",
-            "y",
-            color="red",
-            line_width=2,
-            line_alpha=1,
-            muted_alpha=0.1,
-            source=data,
-        )
+    status = "Service rate"
+    data = ColumnDataSource(data=dict(x=[], y=[]))
+    source_overall_stats[status] = data
+    line = p.line(
+        "x",
+        "y",
+        color=color_overall_status[status],
+        line_width=2,
+        line_alpha=1,
+        muted_alpha=0.1,
+        source=data,
+        y_range_name="Service rate",
+    )
+    items[status] = [line]
 
-        items[status] = [line]
+    status = "Total reward"
+    data = ColumnDataSource(data=dict(x=[], y=[]))
+    source_overall_stats[status] = data
+    line = p.line(
+        "x",
+        "y",
+        color=color_overall_status[status],
+        line_width=2,
+        line_alpha=1,
+        muted_alpha=0.1,
+        source=data,
+    )
+
+    items[status] = [line]
     print(items)
     # Setup legend
     # https://bokeh.pydata.org/en/latest/docs/user_guide/styling.html#legends
@@ -256,30 +315,8 @@ def configure_overall_stats(p, status_list):
         # title="Vehicle status",
     )
 
-    # Setup title
-    title = Title(
-        align="center",
-        text="Cost",
-        text_font_size="16pt",
-        text_color="#929292",
-        text_font_style="normal",
-    )
-
     # Add legend outside plot
     p.add_layout(legend, "right")
-
-    # Add title
-    p.title = title
-
-    # Set x axis settings
-    p.xaxis.axis_label = "Iteration"
-    p.xaxis.major_label_text_font_size = axes_font_size
-    p.xaxis.axis_label_text_font_size = axes_font_size
-
-    # Set y axis settings
-    p.yaxis.axis_label = "#Cost"
-    p.yaxis.major_label_text_font_size = axes_font_size
-    p.yaxis.axis_label_text_font_size = axes_font_size
 
 
 def load_episode(e, smooth_sigma=0):
@@ -327,14 +364,28 @@ def load_overall_stats(smooth_sigma=0):
 
     # Loading episode data
     # for status in d.columns.values:
-    for status in ["Total reward"]:
-        count = list(d[status])
-        # Smooth values
-        if smooth_sigma > 0:
-            count = fi.gaussian_filter1d(count, sigma=smooth_sigma)
+    status = "Total reward"
+    count = list(d[status])
+    # Smooth values
+    if smooth_sigma > 0:
+        count = fi.gaussian_filter1d(count, sigma=smooth_sigma)
 
-        e_data_dict = dict(x=iterations, y=count)
-        source_overall_stats[status].data = e_data_dict
+    e_data_dict = dict(x=iterations, y=count)
+    source_overall_stats[status].data = e_data_dict
+    plot_iterations.y_range = Range1d(min(count), max(count))
+
+    status = "Service rate"
+    count = list(d[status])
+    # Smooth values
+    if smooth_sigma > 0:
+        count = fi.gaussian_filter1d(count, sigma=smooth_sigma)
+
+    e_data_dict = dict(x=iterations, y=count)
+    source_overall_stats[status].data = e_data_dict
+
+    plot_iterations.extra_y_ranges["Service rate"] = Range1d(
+        min(count), max(count)
+    )
 
 
 def load_episode_demand(e, smooth_sigma=0):
@@ -380,7 +431,10 @@ def show_fleet_status(episode):
 
     # Check if episode was previously loaded
     if episode not in episode_fleet_dict:
-        load_episode(episode, smooth_sigma=smooth_sigma_fleet)
+        try:
+            load_episode(episode, smooth_sigma=smooth_sigma_fleet)
+        except Exception as e:
+            print(f"Episode {episode} cannot be loaded. Exception: '{e}'.")
 
     else:
         print(f"Showing episode {episode:04}...")
@@ -412,8 +466,11 @@ def show_demand_status(episode):
 
     # Check if episode was previously loaded
     if episode not in episode_demand_dict:
-        load_episode_demand(episode, smooth_sigma=smooth_sigma_demand)
-
+        try:
+            load_episode_demand(episode, smooth_sigma=smooth_sigma_demand)
+        except Exception as e:
+            print(f"Episode {episode} cannot be loaded. Exception: '{e}'.")
+            episode_demand_dict[episode]["service_rate"] = 0
     else:
         print(f"Showing episode {episode:04}...")
         for status, count in episode_demand_dict[episode][
@@ -461,7 +518,7 @@ status_list = [
     "Returning",
     "Total",
 ]
-list_stats = ["Total reward"]
+list_stats = ["Total reward", "Service rate"]
 
 configure_plot_fleet(plot_fleet, status_list)
 configure_plot_demand(plot_demand)

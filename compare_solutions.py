9fef57fdb68972048dbfe5bc3234e911a6206920
brenobeirigo <brenobeirigo@gmail.com>
2020-09-17 06:36:56 +0200

    Create compare_solutions.py

---
+++ compare_solutions.py
@@ -0,0 +1,4 @@
+from tests.ml.SolutionComparison import SolutionComparison
+
+if __name__ == '__main__':
+    comp = SolutionComparison("data/category_comparison/input/discount_factor_comparison.json")
